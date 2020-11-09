527d2b69ddc52b7de3592873fe543cde00cb4887
Carlos Filoteo <c.filoteo12@gmail.com>
2020-11-09 07:58:00 -0700

    Add Rxjs as external to Svelte app

--- packages/nav/src/App.svelte
+++ packages/nav/src/App.svelte
@@ -1,6 +1,19 @@
 <script>
-  import { BehaviorSubject } from 'rxjs'
+  import { onDestroy, onMount } from 'svelte';
   export let name
+  import { fromEvent } from "rxjs";
+  import { sampleTime } from "rxjs/operators";
+
+  let sub;
+
+  onMount(() => {
+    sub = fromEvent(document, "mousemove").pipe(sampleTime(1000)).subscribe(({ screenX, screenY }) => {
+      console.log(`X: ${screenX}, Y: ${screenY}`);
+    })
+  });
+  onDestroy(() => {
+    sub.unsubscribe();
+  });
 </script>
 
 <style>
