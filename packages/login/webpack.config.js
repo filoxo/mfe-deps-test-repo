4d08362a9146d6ff4f331687e47076ed7ba74bd3
Carlos Filoteo <c.filoteo12@gmail.com>
2020-11-09 07:57:36 -0700

    Resolve typescript from webpack config so that analyzer can find the package

--- packages/login/webpack.config.js
+++ packages/login/webpack.config.js
@@ -1,15 +1,17 @@
-const webpackMerge = require("webpack-merge");
-const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
+const webpackMerge = require('webpack-merge')
+const singleSpaDefaults = require('webpack-config-single-spa-react-ts')
 
 module.exports = (webpackConfigEnv, argv) => {
   const defaultConfig = singleSpaDefaults({
-    orgName: "example",
-    projectName: "login",
+    orgName: 'example',
+    projectName: 'login',
     webpackConfigEnv,
     argv,
-  });
+    // This is necessary so that we can resolve the TypeScript module from outside of the project
+    typescriptPath: require.resolve('typescript', { paths: [__dirname] }),
+  })
 
   return webpackMerge.smart(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
-  });
-};
+  })
+}
